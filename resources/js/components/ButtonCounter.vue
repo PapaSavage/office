ad7b3046095bdbd82dc436bceb10ce8599d10463
Falaleev Danil <danilkafalaleev@gmail.com>
2024-08-03 15:21:57 +0300

    init project laravel + vue

---
+++ resources/js/components/ButtonCounter.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <button class="" @click="count++">You clicked me {{ count }} times.</button>
+</template>
